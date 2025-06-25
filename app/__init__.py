8ef79476938f6f9a05ba7aef28138ba4f77111e5
Alexander Mould <alexmould@hotmail.co.uk>
2025-06-25 14:01:57 +0100

    Including protection against CSRF attacks.

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,25 @@
 from flask import Flask, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_wtf import CSRFProtect
 
 # core extensions
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+csrf = CSRFProtect()
 
 from .seed import register_commands
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
+    # initialise extensions
     db.init_app(app)
     login_manager.init_app(app)
+    csrf.init_app(app)
 
     # delay importing models until db is ready to avoid circular imports
     from .models import User
